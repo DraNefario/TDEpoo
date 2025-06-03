cc70a1e3168e010c5da2a92bd5ee9e986bcf356d
Thomas Manussadjian Steinhausser <162912616+DraNefario@users.noreply.github.com>
2025-06-03 11:07:45 -0300

    Add files via upload

---
+++ carros/src/model/Carro.java
@@ -0,0 +1,57 @@
+package model;
+
+public class Carro {
+    private int id;
+    private String modelo;
+    private String marca;
+    private int ano;
+    private String placa;
+
+    public Carro(int id, String modelo, String marca, int ano, String placa) {
+        this.id = id;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.ano = ano;
+        this.placa = placa;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+}
